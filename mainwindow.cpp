0eb218965cdd07ecfc02bea1fa0ca424ab558a86
Joe Farrish <joseph_a_f@hotmail.com>
2021-03-27 14:11:38 -0700

    added functionality in TContainer to MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,10 @@
 #include "ui_mainwindow.h"
 #include <QLabel>
 #include <QWidget>
+#include <QDebug>
+#include <QString>
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(QWidget *parent, QPoint *p) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
@@ -17,20 +19,156 @@ MainWindow::MainWindow(QWidget *parent) :
 
     this->setGeometry(200,200,800,800);
     this->setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
-    QLabel *lab1 = new QLabel("Label1");
-   // QLabel *lab2 = new QLabel("Label2");
-    TContainer *con1 = new TContainer(this, QPoint(10, 10), lab1);
-    TContainer *con2 = new TContainer(this, QPoint(20, 50)/*,lab2*/);
+    this->setFocusPolicy(Qt::ClickFocus);
+    this->setFocus();
+    this->move(*p);
+    MosPos = new QLabel(this);
+    MosPos->setText("uninited");
+    MosPos->setGeometry(400,400,100,100);
+
+    m_infocus = true;
+    m_isEditing = true;
+    this->installEventFilter(parent);
 
 
-    QWidget *test = new QWidget(con2);
-    test->setStyleSheet("background: green;");
-    test->show();
-
-    con2->setChildWidget(test);
 
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::setCursorShape(const QPointF &e_pos)
+{
+    const int diff = 8;
+        if (
+                //Left-Bottom
+                ((e_pos.y() > y() + height() - diff) &&     //Bottom
+                 (e_pos.x() < x() + diff)) ||               //Left
+                //Right-Bottom
+                ((e_pos.y() > y() + height() - diff) &&     //Bottom
+                 (e_pos.x() > x() + width() - diff)) ||     //Right
+                //Left-Top
+                ((e_pos.y() < y() + diff) &&                //Top
+                 (e_pos.x() < x() + diff)) ||               //Left
+                //Right-Top
+                ((e_pos.y() < y() + diff) &&                //Top
+                 (e_pos.x() > x() + width() - diff))        //Right
+                )
+        {
+            //Left-Bottom
+            if ((e_pos.y() > y() + height() - diff) &&      //Bottom
+                    (e_pos.x() < x() + diff))               //Left
+            {
+                mode = RESIZEBL;
+                setCursor(QCursor(Qt::SizeBDiagCursor));
+            }
+            //Right-Bottom
+            if ((e_pos.y() > y() + height() - diff) &&      //Bottom
+                    (e_pos.x() > x() + width() - diff))     //Right
+            {
+                mode = RESIZEBR;
+                setCursor(QCursor(Qt::SizeFDiagCursor));
+            }
+            //Left-Top
+            if ((e_pos.y() < y() + diff) &&                 //Top
+                    (e_pos.x() < x() + diff))               //Left
+            {
+                mode = RESIZETL;
+                setCursor(QCursor(Qt::SizeFDiagCursor));
+            }
+            //Right-Top
+            if ((e_pos.y() < y() + diff) &&                 //Top
+                    (e_pos.x() > x() + width() - diff))     //Right
+            {
+                mode = RESIZETR;
+                setCursor(QCursor(Qt::SizeBDiagCursor));
+            }
+        }
+        // check cursor horizontal position
+        else if ((e_pos.x() < x() + diff) ||                //Left
+                 ((e_pos.x() > x() + width() - diff)))      //Right
+        {
+            if (e_pos.x() < x() + diff) {                   //Left
+                setCursor(QCursor(Qt::SizeHorCursor));
+                mode = RESIZEL;
+            } else {                                        //Right
+                setCursor(QCursor(Qt::SizeHorCursor));
+                mode = RESIZER;
+            }
+        }
+        // check cursor vertical position
+        else if (((e_pos.y() > y() + height() - diff)) ||   //Bottom
+                 (e_pos.y() < y() + diff))                  //Top
+        {
+            if (e_pos.y() < y() + diff) {                   //Top
+                setCursor(QCursor(Qt::SizeVerCursor));
+                mode = RESIZET;
+            } else {                                        //Bottom
+                setCursor(QCursor(Qt::SizeVerCursor));
+                mode = RESIZEB;
+            }
+        } else {
+            setCursor(QCursor(Qt::ArrowCursor));
+            mode = MOVE;
+        }
+
+}
+
+bool MainWindow::eventFiler(QObject *obj, QEvent *evt)
+{
+    if(m_infocus){
+        //this is a good place to pain if you need to
+        return QMainWindow::eventFilter(obj,evt);
+    }
+    return QMainWindow::eventFilter(obj, evt);
+}
+
+
+void MainWindow::keyPressEvent(QKeyEvent *)
+{
+
+}
+
+void MainWindow::focusInEvent(QFocusEvent *e)
+{
+    m_infocus = true;
+    this->installEventFilter(this);
+    this->repaint();
+    emit inFocus(true);
+}
+
+void MainWindow::focusOutEvent(QFocusEvent *e)
+{
+    if(!m_isEditing) return;
+    mode = NONE;
+    emit outFocus(false);
+    m_infocus = false
+;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+     position = QPoint(e->globalPosition().x()-geometry().x(), e->globalPosition().y()-geometry().y());
+     if (!m_isEditing) return;
+         if (!m_infocus) return;
+         if (e->buttons() == Qt::LeftButton) {
+             qDebug() << "LeftButton event...";
+             setCursorShape(e->globalPosition());
+             return;
+         }else if(e->button() == Qt::RightButton) {
+             qDebug() << "RightButton event...";
+             e->accept();
+         }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *e)
+{
+    QMainWindow::mouseReleaseEvent(e);
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *e)
+{
+    qDebug() << "X: " << e->globalPosition().x() << " Y: " << e->globalPosition().y();
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,10 +7,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "tcontainer.h"
+#include <QVBoxLayout>
+#include <QPoint>
+#include <QMouseEvent>
+#include <QLabel>
+
+enum modes{
+   NONE = 0,
+   MOVE = 1,
+   RESIZETL = 2,
+   RESIZET = 3,
+   RESIZETR = 4,
+   RESIZER = 5,
+   RESIZEBR = 6,
+   RESIZEB = 7,
+   RESIZEBL = 8,
+   RESIZEL = 9
+};
 
 namespace Ui {
 class MainWindow;
+
 }
 
 class MainWindow : public QMainWindow
@@ -18,11 +35,31 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    MainWindow(QWidget *parent = 0, QPoint *p = new QPoint);
+  ~MainWindow();
+protected:
+    int mode;
+
+    QPoint position;
+    QVBoxLayout *vLayout;
+    void setCursorShape(const QPointF &e_pos);
+    bool eventFiler(QObject *obj, QEvent *evt);
+    void keyPressEvent(QKeyEvent*e);
+    void focusInEvent(QFocusEvent*e);
+    void focusOutEvent(QFocusEvent*e);
+    void mousePressEvent(QMouseEvent*e);
+    void mouseReleaseEvent(QMouseEvent*e);
+    void mouseMoveEvent(QMouseEvent*e);
+    bool m_infocus;
+    bool m_isEditing;
+
+signals:
+    void inFocus(bool mode);
+    void outFocus(bool mode);
 
 private:
     Ui::MainWindow *ui;
+    QLabel *MosPos;
 };
 
 #endif // MAINWINDOW_H
