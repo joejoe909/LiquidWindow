fd3eb17e3f48960b9667d665b7a54c1452e6d109
Joe Farrish <joseph_a_f@hotmail.com>
2021-05-02 12:00:52 -0700

    template set correctly

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
+#include "LiquidWindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    LiquidWindow w;
     w.show();
     return a.exec();
 }
